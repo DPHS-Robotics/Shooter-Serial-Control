76d00b2137bb4b01fbb3b59516b057089dd75236
QwertzHz <lpulley99@gmail.com>
2016-02-17 17:50:26 -0600

    Added side limit switch compatibility

--- src/Shooter-Serial-Control.ino
+++ src/Shooter-Serial-Control.ino
@@ -3,6 +3,7 @@ const int loaderPins[2] = { 7, 6 }; // Loader solenoid (7 retracts, 6 extends)
 const int latchPins[2] = { 5, 4 }; // Latch solenoid (5 extends, 4 retracts)
 const int backLimitSwitchPin = 10; // Data pin for the back limit switch (triggers as cam locks)
 const int frontLimitSwitchPin = 11; // Data pin for the front limit switch (triggers as loader finishes retracting)
+const int sideLimitSwitchPin = 12; // Data pin for the side limit switch (triggers while bucket is in back)
 const int limitSwitchPowerPins[2] = { 8, 9 }; // Power pins for the limit switches
 const int pressureReaderPowerPin = A2; // Power pin for the pressure gauge
 const int pressureReaderDataPin = A1; // Data pin for the pressure gauge
@@ -17,14 +18,11 @@ const int commandStateRequest = 7; // Reports on the states of certain relays an
 const int commandAssistedRelease = 9; // Initiates an assisted release, or unload
 
 // Wait times
-const int assistedReleaseLoaderTime = 2000; // Time for the loader to reach the bucket during unload
-const int assistedReleaseVacateTime = 500; // Time for the loader and released bucket to vacate the cam during unload
-const int firingSequenceVacateTime = 200; // Time for the sprung bucket to vacate the cam during firing
 const int relayHoldTime = 20; // Time to power solenoids in all cases
 
 // Pressures
-const int fireAtPressure = 70; // Pressure necessary to remove latch from cam
-const int loadAtPressure = 110; // Pressure necessary for loader to load bucket
+const int fireAtPressure = 70; // Pressure necessary to remove latch from cam (unadjusted for volume)
+const int loadAtPressure = 100; // Pressure necessary for loader to load bucket (unadjusted for volume)
 
 int fromSerial; // The parsed integer received from the serial connection
 int loaderState; // The most recently powered pin for the loader
@@ -45,6 +43,7 @@ void setup()
   }
   pinMode(backLimitSwitchPin, INPUT); // Back limit switch data
   pinMode(frontLimitSwitchPin, INPUT); // Front limit switch data
+  pinMode(sideLimitSwitchPin, INPUT); // Side limit switch data
   pinMode(pressureReaderGroundPin, OUTPUT); // Pressure gauge ground pin
   pinMode(pressureReaderDataPin, INPUT); // Pressure gauge data pin
   pinMode(pressureReaderPowerPin, OUTPUT); // Pressure gauge power pin
@@ -100,30 +99,16 @@ void runCommand(int command)
     holdForPressure(loadAtPressure);
     Serial.println("...pushing loading slide..."); // Once we are at pressure, we can load
     absoluteRelay(commandLoader, 1);
-    while (!digitalRead(backLimitSwitchPin)) // Wait while the loader has not finished loading the bucket
-    {
-      if (Serial.parseInt() != 0) // Allow cancelation of the firing sequence
-      {
-        Serial.println("...canceled!!");
-        return;
-      }
-    }
+    while (!digitalRead(backLimitSwitchPin)); // Wait while the loader has not finished loading the bucket
     Serial.println("...resetting loading slide..."); // The bucket has been loaded and the loader can retract
     absoluteRelay(commandLoader, 0);
-    while (!digitalRead(frontLimitSwitchPin)) // Wait while the loader has not finished retracting
-    {
-      if (Serial.parseInt() != 0) // Allow cancelation of the firing sequence
-      {
-        Serial.println("...canceled!!");
-        return;
-      }
-    }
+    while (!digitalRead(frontLimitSwitchPin)); // Wait while the loader has not finished retracting
     Serial.println("...waiting for pressure..."); // We must have fireAtPressure PSI in order to successfully release the latch
     holdForPressure(fireAtPressure);
     Serial.println("...releasing latch..."); // We can now pull the latch from the cam
     absoluteRelay(commandLatch, 1);
-    Serial.println("...waiting for bucket to vacate..."); // Give the bucket time to get out of the cam before relocking
-    delay(firingSequenceVacateTime);
+    Serial.println("...waiting for bucket to vacate..."); // Wait for the bucket to leave the back end
+    while(digitalRead(sideLimitSwitchPin));
     Serial.println("...locking latch..."); // Relock the latch for the next firing sequence
     absoluteRelay(commandLatch, 0);
     Serial.println("...firing sequence complete.");
@@ -145,17 +130,17 @@ void runCommand(int command)
     Serial.println("Beginning assisted release...");
     Serial.println("...waiting for pressure..."); // We need loadAtPressure PSI to ensure loader strength
     holdForPressure(loadAtPressure);
-    Serial.println("...pushing loading slide and waiting..."); // Move the loader to hold the bucket
+    Serial.println("...pushing loading slide..."); // Move the loader to hold the bucket
     absoluteRelay(commandLoader, 1);
-    delay(assistedReleaseLoaderTime); // The loader needs time to get to the bucket
+    while(!digitalRead(backLimitSwitchPin)); // Wait for the bucket to be held back by the loader
     Serial.println("...waiting for pressure..."); // We need fireAtPressure PSI to ensure latch release
     holdForPressure(fireAtPressure);
     Serial.println("...releasing latch..."); // Release the latch
     absoluteRelay(commandLatch, 1);
     Serial.println("...releasing loader..."); // Let the loader carry the bucket back to the front
     absoluteRelay(commandLoader, 0);
-    Serial.println("...waiting for loader to vacate..."); // Give the bucket time to vacate the cam
-    delay(assistedReleaseVacateTime);
+    Serial.println("...waiting for bucket to vacate..."); // Wait for the bucket to leave the back end
+    while (digitalRead(sideLimitSwitchPin));
     Serial.println("...locking latch..."); // Relock the latch for the next fire or unload
     absoluteRelay(commandLatch, 0);
     Serial.println("...assisted release complete.");
